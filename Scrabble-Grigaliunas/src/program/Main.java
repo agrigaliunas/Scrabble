9bd8d84850333ab4cfdb5874809bfb9b56238d79
agrigaliunas <agustingrigaliunas@gmail.com>
2021-09-25 19:24:10 -0300

    generarFichas method

--- Scrabble-Grigaliunas/src/program/Main.java
+++ Scrabble-Grigaliunas/src/program/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Juego juego = new Juego();
-		//juego.configurarJugadores();
+		juego.configurarJugadores();
 		juego.generarFichas();
 				
 	}
